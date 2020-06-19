8135e5f6bdb3d2224c3c50508f84fcb70337d8a9
Elise <elise.piazza@gmail.com>
2020-06-19 15:55:48 -0400

    Added preproc param loop

--- ep_computeCorrClass.m
+++ ep_computeCorrClass.m
@@ -10,99 +10,103 @@
 clear;
 subjects = [103 105 108 115 117 120 121 122 123]; nSubs = length(subjects);
 
-preproc_type = 'AFNI'; %'AFNI', 'Python'
-preproc_params = 'v7_15_regressors_no_smoothing_defaultGMmask_polort=2';
-%AFNI parameter choices:
-%v1_original_regressors
-%v2_jamals_regressors
-%v3_jamals_regressors_smoothing=1
-%v4_jamals_regressors_smoothing=1_defaultGMmask
-%v5_jamals_regressors_smoothing=1_defaultGMmask_polort=3
-%v6_jamals_regressors_smoothing=1_defaultGMmask_polort=2
-%v7_15_regressors_no_smoothing_defaultGMmask_polort=2
-%Python parameter choices:
-%HPF=.01Hz, HPF=.03Hz, HPF=.06Hz
+preproc_types = {'AFNI', 'AFNI', 'AFNI', 'AFNI', 'AFNI', 'AFNI', 'AFNI', 'Python', 'Python', 'Python'};
+preproc_params = {'v1_original_regressors', 'v2_jamals_regressors', 'v3_jamals_regressors_smoothing=1', ...
+    'v4_jamals_regressors_smoothing=1_defaultGMmask', 'v5_jamals_regressors_smoothing=1_defaultGMmask_polort=3', ...
+    'v6_jamals_regressors_smoothing=1_defaultGMmask_polort=2', 'v7_15_regressors_no_smoothing_defaultGMmask_polort=2', ...
+    'HPF=.01Hz', 'HPF=.03Hz', 'HPF=.06Hz'};
 
-for s = 1:nSubs
-    subject = subjects(s);
+n_cropped_TRs = 10;
+
+for p = 1:length(preproc_types)
     
-    load(['../../common_space_' preproc_type '/reshaped_by_conditions/' preproc_params '/sub-' num2str(subject) '.mat']);
-    n_scramble_cond = size(data_ROIavg_scramble,3); n_scramble_reps = size(data_ROIavg_scramble,4);
-    choose = @(samples) samples(randi(numel(samples)));
+    preproc_type = preproc_types{p};
+    preproc_param = preproc_params{p};
     
-    nROIs = length(ROIs);
-    
-    %Crop N TRs from beginning and end
-    n_cropped_TRs = 10;
-    data_ROIavg_scramble = data_ROIavg_scramble(:,n_cropped_TRs+1:end-n_cropped_TRs,:,:);
-    data_ROIavg_control = data_ROIavg_control(:,n_cropped_TRs+1:end-n_cropped_TRs,:,:);
-    
-    for ROI = 1:nROIs
+    for s = 1:nSubs
+        subject = subjects(s);
         
-        data_scramble_thisROI = data_scramble{ROI}; %The extracted 4D matrix should be V x T x cond x rep
+        load(['../../common_space_' preproc_type '/reshaped_by_conditions/' preproc_param '/sub-' num2str(subject) '.mat']);
+        n_scramble_cond = size(data_ROIavg_scramble,3); n_scramble_reps = size(data_ROIavg_scramble,4);
+        choose = @(samples) samples(randi(numel(samples)));
         
-        held_out_reps = randperm(n_scramble_reps);
+        nROIs = length(ROIs);
         
-        for i = 1:n_scramble_reps
+        %Crop N TRs from beginning and end
+        data_ROIavg_scramble = data_ROIavg_scramble(:,n_cropped_TRs+1:end-n_cropped_TRs,:,:);
+        data_ROIavg_control = data_ROIavg_control(:,n_cropped_TRs+1:end-n_cropped_TRs,:,:);
+        
+        for ROI = 1:nROIs
             
-            test_rep = held_out_reps(i);
+            data_scramble_thisROI = data_scramble{ROI}; %The extracted 4D matrix should be V x T x cond x rep
             
-            %Average of 1B training reps (VxT)
-            train_1B = mean(data_scramble_thisROI(:,:,1,setdiff([1:n_scramble_reps],test_rep)),4);
+            held_out_reps = randperm(n_scramble_reps);
             
-            %Average of 2B training reps (VxT)
-            train_2B = mean(data_scramble_thisROI(:,:,2,setdiff([1:n_scramble_reps],test_rep)),4);
+            for i = 1:n_scramble_reps
+                
+                test_rep = held_out_reps(i);
+                
+                %Average of 1B training reps (VxT)
+                train_1B = mean(data_scramble_thisROI(:,:,1,setdiff([1:n_scramble_reps],test_rep)),4);
+                
+                %Average of 2B training reps (VxT)
+                train_2B = mean(data_scramble_thisROI(:,:,2,setdiff([1:n_scramble_reps],test_rep)),4);
+                
+                %Average of 8B training reps (VxT)
+                train_8B = mean(data_scramble_thisROI(:,:,3,setdiff([1:n_scramble_reps],test_rep)),4);
+                
+                %Average of I training reps (VxT)
+                train_I = mean(data_scramble_thisROI(:,:,4,setdiff([1:n_scramble_reps],test_rep)),4);
+                
+                %Held-out 1B run (VxT)
+                test_1B = data_scramble_thisROI(:,:,1,test_rep);
+                
+                %Held-out 2B run (VxT)
+                test_2B = data_scramble_thisROI(:,:,2,test_rep);
+                
+                %Held-out 8B run (VxT)
+                test_8B = data_scramble_thisROI(:,:,3,test_rep);
+                
+                %Held-out I run (VxT)
+                test_I = data_scramble_thisROI(:,:,4,test_rep);
+                
+                %Is train_1B most strongly correlated with its own held-out (test) loaf than the other 3?
+                R1 = corrcoef(train_1B(:,:),test_1B(:,:)); R2 = corrcoef(train_1B(:,:),test_I(:,:)); R3 = corrcoef(train_1B(:,:),test_8B(:,:)); R4 = corrcoef(train_1B(:,:),test_2B(:,:));
+                acc1 = R1(2,1) > [R2(2,1) R3(2,1) R4(2,1)]; acc_1B(i) = sum(acc1) == 3;
+                
+                %Is train_2B most strongly correlated with its own held-out (test) loaf than the other 3?
+                R1 = corrcoef(train_2B(:,:),test_2B(:,:)); R2 = corrcoef(train_2B(:,:),test_I(:,:)); R3 = corrcoef(train_2B(:,:),test_8B(:,:)); R4 = corrcoef(train_2B(:,:),test_1B(:,:));
+                acc1 = R1(2,1) > [R2(2,1) R3(2,1) R4(2,1)]; acc_2B(i) = sum(acc1) == 3;
+                
+                %Is train_8B most strongly correlated with its own held-out (test) loaf than the other 3?
+                R1 = corrcoef(train_8B(:,:),test_8B(:,:)); R2 = corrcoef(train_8B(:,:),test_I(:,:)); R3 = corrcoef(train_8B(:,:),test_2B(:,:)); R4 = corrcoef(train_8B(:,:),test_1B(:,:));
+                acc1 = R1(2,1) > [R2(2,1) R3(2,1) R4(2,1)]; acc_8B(i) = sum(acc1) == 3;
+                
+                %Is train_1 most strongly correlated with its own held-out (test) loaf than the other 3?
+                R1 = corrcoef(train_I(:,:),test_I(:,:)); R2 = corrcoef(train_I(:,:),test_8B(:,:)); R3 = corrcoef(train_I(:,:),test_2B(:,:)); R4 = corrcoef(train_I(:,:),test_1B(:,:));
+                acc1 = R1(2,1) > [R2(2,1) R3(2,1) R4(2,1)]; acc_I(i) = sum(acc1) == 3;
+            end
             
-            %Average of 8B training reps (VxT)
-            train_8B = mean(data_scramble_thisROI(:,:,3,setdiff([1:n_scramble_reps],test_rep)),4);
+            ROI_acc(ROI,1) = mean(acc_1B);
+            ROI_acc(ROI,2) = mean(acc_2B);
+            ROI_acc(ROI,3) = mean(acc_8B);
+            ROI_acc(ROI,4) = mean(acc_I);
             
-            %Average of I training reps (VxT)
-            train_I = mean(data_scramble_thisROI(:,:,4,setdiff([1:n_scramble_reps],test_rep)),4);
-            
-            %Held-out 1B run (VxT)
-            test_1B = data_scramble_thisROI(:,:,1,test_rep);
-            
-            %Held-out 2B run (VxT)
-            test_2B = data_scramble_thisROI(:,:,2,test_rep);
-            
-            %Held-out 8B run (VxT)
-            test_8B = data_scramble_thisROI(:,:,3,test_rep);
-            
-            %Held-out I run (VxT)
-            test_I = data_scramble_thisROI(:,:,4,test_rep);
-            
-            %Is train_1B most strongly correlated with its own held-out (test) loaf than the other 3?
-            R1 = corrcoef(train_1B(:,:),test_1B(:,:)); R2 = corrcoef(train_1B(:,:),test_I(:,:)); R3 = corrcoef(train_1B(:,:),test_8B(:,:)); R4 = corrcoef(train_1B(:,:),test_2B(:,:));
-            acc1 = R1(2,1) > [R2(2,1) R3(2,1) R4(2,1)]; acc_1B(i) = sum(acc1) == 3;
-            
-            %Is train_2B most strongly correlated with its own held-out (test) loaf than the other 3?
-            R1 = corrcoef(train_2B(:,:),test_2B(:,:)); R2 = corrcoef(train_2B(:,:),test_I(:,:)); R3 = corrcoef(train_2B(:,:),test_8B(:,:)); R4 = corrcoef(train_2B(:,:),test_1B(:,:));
-            acc1 = R1(2,1) > [R2(2,1) R3(2,1) R4(2,1)]; acc_2B(i) = sum(acc1) == 3;
-            
-            %Is train_8B most strongly correlated with its own held-out (test) loaf than the other 3?
-            R1 = corrcoef(train_8B(:,:),test_8B(:,:)); R2 = corrcoef(train_8B(:,:),test_I(:,:)); R3 = corrcoef(train_8B(:,:),test_2B(:,:)); R4 = corrcoef(train_8B(:,:),test_1B(:,:));
-            acc1 = R1(2,1) > [R2(2,1) R3(2,1) R4(2,1)]; acc_8B(i) = sum(acc1) == 3;
-            
-            %Is train_1 most strongly correlated with its own held-out (test) loaf than the other 3?
-            R1 = corrcoef(train_I(:,:),test_I(:,:)); R2 = corrcoef(train_I(:,:),test_8B(:,:)); R3 = corrcoef(train_I(:,:),test_2B(:,:)); R4 = corrcoef(train_I(:,:),test_1B(:,:));
-            acc1 = R1(2,1) > [R2(2,1) R3(2,1) R4(2,1)]; acc_I(i) = sum(acc1) == 3;
         end
         
-        ROI_acc(ROI,1,s) = mean(acc_1B);
-        ROI_acc(ROI,2,s) = mean(acc_2B);
-        ROI_acc(ROI,3,s) = mean(acc_8B);
-        ROI_acc(ROI,4,s) = mean(acc_I);
+        %Generate a figure of classification accuracy (ROI x cond) for this subject 
+        if strcmp(preproc_type, 'AFNI')
+            figsize = [100 100 400 500];
+        elseif strcmp(preproc_type, 'Python')
+            figsize = [100 100 400 350];
+        end
         
+        figure('Units', 'pixels', 'Position', figsize); imagesc(ROI_acc); xlabel('Condition'); ylabel('ROI'); set(gca, 'XTickLabel', scramble_conditions, 'YTickLabel', ROIs, 'FontSize', 16, 'FontName', 'Helvetica'); colorbar; caxis([0 1]);
+        print(gcf, '-dtiff', ['../figures/CC/sub-' num2str(subject) '_' preproc_type '_' preproc_param '_nTRs_cropped=' num2str(n_cropped_TRs) '.tif']);
+        
+%         avg_acc(p,s) = mean(mean(ROI_acc));
     end
     
 end
 
-% if strcmp(preproc_type, 'AFNI')
-%     figsize = [100 100 400 500];
-% elseif strcmp(preproc_type, 'Python')
-%     figsize = [100 100 400 350];
-% end
-%
-% figure('Units', 'pixels', 'Position', figsize); imagesc(ROI_acc); xlabel('Condition'); ylabel('ROI'); set(gca, 'XTickLabel', scramble_conditions, 'YTickLabel', ROIs, 'FontSize', 16, 'FontName', 'Helvetica'); colorbar; caxis([0 1]);
-% print(gcf, '-dtiff', ['../figures/Correlation classifier/sub-' num2str(subject) '_' preproc_type '_' preproc_params '_nTRs_cropped=' num2str(n_cropped_TRs) '.tif']);
-%
+
