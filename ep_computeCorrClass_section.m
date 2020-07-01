c77c09f19f71ace7e23a42232adbe3482f57f212
Elise <elise.piazza@gmail.com>
2020-07-01 17:53:15 -0400

    Fixed summary figure

--- ep_computeCorrClass_section.m
+++ ep_computeCorrClass_section.m
@@ -18,6 +18,10 @@ starts = [1 41 78 107] + 2;
 lengths = 30;
 n_sections = 4;
 
+%Initialize empty matrices of accuracies
+ROI_acc_sections = zeros(10,4,nSubs); %nROIs x nSections x nSubs
+
+
 for s = 1:nSubs
     subject = subjects(s);
     
@@ -25,9 +29,6 @@ for s = 1:nSubs
     n_scramble_reps = size(data_ROIavg_scramble,4);
     
     nROIs = length(ROIs);
-    
-    %Initialize empty matrices of accuracies
-    ROI_acc_sections = zeros(nROIs,n_sections);
     
     for ROI = 1:nROIs
         
@@ -47,10 +48,10 @@ for s = 1:nSubs
             
             %Average of section 3 training reps (VxT)
             train_3 = mean(data_scramble_thisROI(:,starts(3):starts(3)+lengths-1,4,setdiff([1:n_scramble_reps],test_rep)),4);
-
+            
             %Average of section 4 training reps (VxT)
             train_4 = mean(data_scramble_thisROI(:,starts(4):starts(4)+lengths-1,4,setdiff([1:n_scramble_reps],test_rep)),4);
-
+            
             %Held-out section 1 rep (VxT)
             test_1 = data_scramble_thisROI(:,starts(1):starts(1)+lengths-1,4,test_rep);
             
@@ -78,19 +79,26 @@ for s = 1:nSubs
             %Is train_4 most strongly correlated with its own held-out (test) loaf than the other 3?
             R1 = corrcoef(train_4(:,:),test_4(:,:)); R2 = corrcoef(train_4(:,:),test_1(:,:)); R3 = corrcoef(train_4(:,:),test_2(:,:)); R4 = corrcoef(train_4(:,:),test_3(:,:));
             acc1 = R1(2,1) > [R2(2,1) R3(2,1) R4(2,1)]; acc_4(i) = sum(acc1) == n_sections-1;
-         
+            
         end
         
-        ROI_acc_sections(ROI,1) = mean(acc_1);
-        ROI_acc_sections(ROI,2) = mean(acc_2);
-        ROI_acc_sections(ROI,3) = mean(acc_3);
-        ROI_acc_sections(ROI,4) = mean(acc_4);
+        ROI_acc_sections(ROI,1,s) = mean(acc_1);
+        ROI_acc_sections(ROI,2,s) = mean(acc_2);
+        ROI_acc_sections(ROI,3,s) = mean(acc_3);
+        ROI_acc_sections(ROI,4,s) = mean(acc_4);
     end
     
     %Plot classification accuracy (ROI x cond) for this subject (all 4 sections)
-    figsize = [100 100 400 500]; figure('Units', 'pixels', 'Position', figsize); 
-    imagesc(ROI_acc_sections); xlabel('Section'); ylabel('ROI'); set(gca, 'YTickLabel', ROIs, 'FontSize', 16, 'FontName', 'Helvetica'); colorbar; caxis([0 1]);
-    print(gcf, '-dtiff', ['../figures/Correlation classifier/sub-' num2str(subject) '_sections_' preproc_type '_' preproc_param '.tif']);
+    figsize = [100 100 400 500];
+    figure('Units', 'pixels', 'Position', figsize); imagesc(ROI_acc_sections(:,:,s)); xlabel('Section'); ylabel('ROI'); set(gca, 'YTickLabel', ROIs, 'FontSize', 16, 'FontName', 'Helvetica'); colorbar; caxis([0 1]);
+    print(gcf, '-dtiff', ['../figures/Correlation classifier/sub-' num2str(subject) '_sections_' preproc_type '_' preproc_param '1.tif']);
     
 end
+
+%Plot classification accuracy, averaged across subjects (ROI x section)
+figsize = [100 100 300 400];
+figure('Units', 'pixels', 'Position', figsize);
+imagesc(mean(ROI_acc_sections,3));
+title('Classify sections'); xlabel('Section'); ylabel('ROI'); set(gca, 'FontSize', 16, 'FontName', 'Helvetica', 'YTickLabel', ROIs); colorbar; caxis([0 1]);
+print(gcf, '-dtiff', ['../figures/Correlation classifier/Summary by ROI_section_1.tif']);
 
