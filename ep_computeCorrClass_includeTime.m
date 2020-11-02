6632615f247947ca2c637361f6265cabb038ba2d
Elise <elise.piazza@gmail.com>
2020-11-02 18:11:23 -0500

    Adapted for this folder

--- ep_computeCorrClass_includeTime.m
+++ ep_computeCorrClass_includeTime.m
@@ -1,9 +1,9 @@
 %ep_computeCorrClass_includeTime
 %For each subject, for each ROI, for each condition, for each rep:
-%1) Create the training pattern (VxT matrix, avg'd across N-1 reps).  
+%1) Create the training pattern (VxT matrix, avg'd across N-1 reps).
 %This gives you 4 training patterns (1 for each condition). The 4 test patterns are the held-out patterns (rep 1).
-%For each of the 4 test patterns, compute the corr between that and all 4 training patterns. E.g., for 1B, [.8 .2 .3 .3]. 
-%Since the highest correlation is the first, you predict condition 1B. Do this for all 4 conditions, and you get 4 predictions 
+%For each of the 4 test patterns, compute the corr between that and all 4 training patterns. E.g., for 1B, [.8 .2 .3 .3].
+%Since the highest correlation is the first, you predict condition 1B. Do this for all 4 conditions, and you get 4 predictions
 %(e.g., [1B 2B 2B I]). Your accuracy (prop. correct) for this fold is 75%.
 %Do this for all 3 reps (folds) and you get a total of 3 values. Average across them to get a single value.
 
@@ -26,11 +26,11 @@ end
 for s = 1:length(subjects)
     subject = subjects(s);
     
-    load(['../reshaped_by_conditions/' data_type '/sub-' num2str(subject) '.mat']);
+    load(['../../common_space_AFNI/reshaped_by_conditions/' data_type '/sub-' num2str(subject) '.mat']);
     n_scramble_reps = size(data_ROIavg_scramble,4);
     
     nROIs = length(ROIs);
-        
+    
     for ROI = 1:nROIs
         
         data_scramble_thisROI = data_scramble{ROI}; %The extracted 4D matrix should be V x T x cond x rep
@@ -70,28 +70,28 @@ for s = 1:length(subjects)
             
             %Compute the correlations between test_1B and all 4 training patterns
             R1 = corrcoef(test_1B(:,:),train_1B(:,:)); R2 = corrcoef(test_1B(:,:),train_2B(:,:)); R3 = corrcoef(test_1B(:,:),train_8B(:,:)); R4 = corrcoef(test_1B(:,:),train_I(:,:));
-            accs = [R1(2,1) R2(2,1) R3(2,1) R4(2,1)]; 
+            accs = [R1(2,1) R2(2,1) R3(2,1) R4(2,1)];
             %Check whether the max corr is w/ 1B's training pattern
             [m, el] = max(accs); acc_1B = el==1;
             
             %Compute the correlations between test_2B and all 4 training patterns
             R1 = corrcoef(test_2B(:,:),train_1B(:,:)); R2 = corrcoef(test_2B(:,:),train_2B(:,:)); R3 = corrcoef(test_2B(:,:),train_8B(:,:)); R4 = corrcoef(test_2B(:,:),train_I(:,:));
-            accs = [R1(2,1) R2(2,1) R3(2,1) R4(2,1)]; 
+            accs = [R1(2,1) R2(2,1) R3(2,1) R4(2,1)];
             %Check whether the max corr is w/ 2B's training pattern
             [m, el] = max(accs); acc_2B = el==2;
-
+            
             %Compute the correlations between test_8B and all 4 training patterns
             R1 = corrcoef(test_8B(:,:),train_1B(:,:)); R2 = corrcoef(test_8B(:,:),train_2B(:,:)); R3 = corrcoef(test_8B(:,:),train_8B(:,:)); R4 = corrcoef(test_8B(:,:),train_I(:,:));
-            accs = [R1(2,1) R2(2,1) R3(2,1) R4(2,1)]; 
+            accs = [R1(2,1) R2(2,1) R3(2,1) R4(2,1)];
             %Check whether max corr is w/ 8B's training pattern
             [m, el] = max(accs); acc_8B = el==3;
-
+            
             %Compute the correlations between test_I and all 4 training patterns
             R1 = corrcoef(test_I(:,:),train_1B(:,:)); R2 = corrcoef(test_I(:,:),train_2B(:,:)); R3 = corrcoef(test_I(:,:),train_8B(:,:)); R4 = corrcoef(test_I(:,:),train_I(:,:));
-            accs = [R1(2,1) R2(2,1) R3(2,1) R4(2,1)]; 
+            accs = [R1(2,1) R2(2,1) R3(2,1) R4(2,1)];
             %Check whether max corr is w/ I's training pattern
             [m, el] = max(accs); acc_I = el==4;
-
+            
             %Store the average classification acc (across conditions) for
             %this held-out rep
             avg_acc(i) = mean([acc_1B acc_2B acc_8B acc_I]);
@@ -101,9 +101,9 @@ for s = 1:length(subjects)
         ROI_acc(ROI) = mean(avg_acc);
     end
     
-%     figsize = [100 100 400 500];
-%     figure('Units', 'pixels', 'Position', figsize); imagesc(ROI_acc); xlabel('Condition'); ylabel('ROI'); set(gca, 'XTickLabel', scramble_conditions, 'YTickLabel', ROIs, 'FontSize', 16, 'FontName', 'Helvetica'); colorbar; caxis([0 1]);
-%     print(gcf, '-dtiff', ['../figures/sub-' num2str(subject) '/Corr classifier_' data_type '.tif']);
+    %     figsize = [100 100 400 500];
+    %     figure('Units', 'pixels', 'Position', figsize); imagesc(ROI_acc); xlabel('Condition'); ylabel('ROI'); set(gca, 'XTickLabel', scramble_conditions, 'YTickLabel', ROIs, 'FontSize', 16, 'FontName', 'Helvetica'); colorbar; caxis([0 1]);
+    %     print(gcf, '-dtiff', ['../figures/sub-' num2str(subject) '/Corr classifier_' data_type '.tif']);
     
     ROI_acc_allSubs(:,s) = ROI_acc;
     
