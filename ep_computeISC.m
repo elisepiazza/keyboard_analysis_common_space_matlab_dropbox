be9472927bef370c3a1de3fdad4715da09b6dfcf
Elise <elise.piazza@gmail.com>
2020-11-08 21:00:15 -0500

    Added new ROIs

--- ep_computeISC.m
+++ ep_computeISC.m
@@ -6,27 +6,30 @@ clear;
 group = 'AM';
 
 %The exact reps you want to include
-scramble_reps_to_include = [1 2 3]; control_reps_to_include = [1 2]; 
+scramble_reps_to_include = [3]; control_reps_to_include = [1 2];
 
 preproc_type = 'AFNI'; preproc_params = 'v7_15_regressors_no_smoothing_defaultGMmask_polort=2';
 
-n_cropped_TRs = 0;
+n_cropped_TRs = 10;
 
-all_subjects = [103 105 115 117 120 121 122 123]; 
-groups = {'AM', 'M', 'AM', 'M', 'AM', 'M', 'M', 'AM'}; 
+% all_subjects = [103 105 115 117 120 121 122 123];
+% groups = {'AM', 'M', 'AM', 'M', 'AM', 'M', 'M', 'AM'};
 
 
-% all_subjects = [103 105 108 115 117 120 121 122 123]; 
-% groups = {'AM', 'M', 'M', 'AM', 'M', 'AM', 'M', 'M', 'AM'}; 
+all_subjects = [103 105 108 115 117 120 121 122 123];
+groups = {'AM', 'M', 'M', 'AM', 'M', 'AM', 'M', 'M', 'AM'};
 
 subjects = all_subjects(find(strcmp(groups,group))); nSubs = length(subjects);
 
 if strcmp(preproc_type, 'AFNI')
+    %     ROIs = {'AngularG', 'Cerebellum', 'HeschlsG', 'STG', 'MotorCortex', ...
+    %     'TPJ', 'PCC', 'Precuneus', 'A1', 'mPFC'};
     ROIs = {'AngularG', 'Cerebellum', 'HeschlsG', 'STG', 'MotorCortex', ...
-    'TPJ', 'PCC', 'Precuneus', 'A1', 'mPFC'};
-    filepath = ['../../common_space_AFNI/reshaped_by_conditions/' preproc_params '/sub-'];
+        'TPJ', 'PCC', 'Precuneus', 'A1', 'mPFC', 'Hipp', 'lTPJ', 'rTPJ', 'PMC', 'V1'};
+    
+    filepath = ['../../common_space_AFNI/reshaped_by_conditions/' preproc_params '/nROIs=15/sub-'];
     barcolor = [.9 .5 0];
-
+    
 elseif strcmp(preproc_type, 'Python')
     ROIs = {'A1', 'AngularGyrus', 'Erez-DMN', 'Precuneus', 'vmPFC'};
     filepath = ['../../common_space_Python/reshaped_by_conditions/' preproc_params '/sub-'];
@@ -51,9 +54,9 @@ for s = 1:nSubs
     elseif strcmp(preproc_type,'Python')
         load([filepath num2str(subjects(s)) '.mat']);
     end
-            
-    data_ROIavg_scramble_allSubs(:,:,:,:,s) = data_ROIavg_scramble; 
-    data_ROIavg_control_allSubs(:,:,:,:,s) = data_ROIavg_control; 
+    
+    data_ROIavg_scramble_allSubs(:,:,:,:,s) = data_ROIavg_scramble;
+    data_ROIavg_control_allSubs(:,:,:,:,s) = data_ROIavg_control;
 end
 
 %Initialize empty ISC matrices
@@ -79,13 +82,13 @@ for ROI = 1:nROIs
             ISC_mat_scramble(ROI,cond,s) = ISC_r(2,1);
         end
         
-%         figure('Units', 'pixels', 'Position', [100 100 1000 375]);
-%         plot(currSubData,'LineWidth',2); hold on; plot(squeeze(otherSubsData),'LineWidth',2);
-%         xlabel('TR'); ylabel('BOLD'); title([ROIs{ROI} ' time series (' conditions{cond} '), ' group ' group, crop=' num2str(n_cropped_TRs)]); ylim([-5 5]); set(gca,'FontSize',16);
-%         print(gcf, '-dtiff', ['../figures/Time series (' conditions{cond} '_' ROIs{ROI} ')_' group ' group, crop=' num2str(n_cropped_TRs) '_' AFNI_data_type '.tif']);       
+        %         figure('Units', 'pixels', 'Position', [100 100 1000 375]);
+        %         plot(currSubData,'LineWidth',2); hold on; plot(squeeze(otherSubsData),'LineWidth',2);
+        %         xlabel('TR'); ylabel('BOLD'); title([ROIs{ROI} ' time series (' conditions{cond} '), ' group ' group, crop=' num2str(n_cropped_TRs)]); ylim([-5 5]); set(gca,'FontSize',16);
+        %         print(gcf, '-dtiff', ['../figures/Time series (' conditions{cond} '_' ROIs{ROI} ')_' group ' group, crop=' num2str(n_cropped_TRs) '_' AFNI_data_type '.tif']);
     end
     
-     %For control conditions
+    %For control conditions
     for cond = 1:n_control_cond
         for s = 1:nSubs
             otherSubs = setdiff(1:nSubs,s);
@@ -101,45 +104,45 @@ for ROI = 1:nROIs
             ISC_mat_control(ROI,cond,s) = ISC_r(2,1);
         end
         
-%         figure('Units', 'pixels', 'Position', [100 100 1000 375]);
-%         plot(currSubData,'LineWidth',2); hold on; plot(squeeze(otherSubsData),'LineWidth',2);
-%         xlabel('TR'); ylabel('BOLD'); title([ROIs{ROI} ' time series (' conditions{cond} '), ' group ' group, crop=' num2str(n_cropped_TRs)]); ylim([-5 5]); set(gca,'FontSize',16);
-%         print(gcf, '-dtiff', ['../figures/Time series (' conditions{cond} '_' ROIs{ROI} ')_' group ' group, crop=' num2str(n_cropped_TRs) '_' AFNI_data_type '.tif']);       
+        %         figure('Units', 'pixels', 'Position', [100 100 1000 375]);
+        %         plot(currSubData,'LineWidth',2); hold on; plot(squeeze(otherSubsData),'LineWidth',2);
+        %         xlabel('TR'); ylabel('BOLD'); title([ROIs{ROI} ' time series (' conditions{cond} '), ' group ' group, crop=' num2str(n_cropped_TRs)]); ylim([-5 5]); set(gca,'FontSize',16);
+        %         print(gcf, '-dtiff', ['../figures/Time series (' conditions{cond} '_' ROIs{ROI} ')_' group ' group, crop=' num2str(n_cropped_TRs) '_' AFNI_data_type '.tif']);
     end
     
     
     %Plot ISC for scramble conditions
-    x = 1:n_scramble_cond; 
+    x = 1:n_scramble_cond;
     %Reshape into sub x cond data
-    data_to_plot = squeeze(ISC_mat_scramble(ROI,:,:))'; 
+    data_to_plot = squeeze(ISC_mat_scramble(ROI,:,:))';
     y = mean(data_to_plot);
     errors = std(data_to_plot)/sqrt(nSubs);
     
-    figsize = [100 100 400 375]; barwidth = .5; 
+    figsize = [100 100 400 375]; barwidth = .5;
     figure('Units', 'pixels', 'Position', figsize);
     bar(x,y,barwidth,'facecolor', barcolor); hold on;
     errorbar(x,y,errors,'k.', 'LineWidth', 1)
     
     xlabel('Condition'); ylabel('ISC by condition (r)'); title([ROIs{ROI}]); xlim([.3 4.7]); ylim([0 .6]); set(gca, 'XTickLabel', scramble_conditions, 'FontSize', 16, 'FontName', 'Helvetica');
     print(gcf, '-dtiff', ['../figures/ISC/ISC by condition (Scramble conds), rep' num2str(scramble_reps_to_include) '_' group ' group, (' ROIs{ROI} ')_' preproc_type '_' preproc_params '_nTRs_cropped=' num2str(n_cropped_TRs) '.tif']);
-
+    
     
     %Plot ISC for control conditions
-    x = 1:n_control_cond; 
+    x = 1:n_control_cond;
     %Reshape into sub x cond data
-    data_to_plot = squeeze(ISC_mat_control(ROI,:,:))'; 
+    data_to_plot = squeeze(ISC_mat_control(ROI,:,:))';
     y = mean(data_to_plot);
     errors = std(data_to_plot)/sqrt(nSubs);
-%     y = nanmean(ISC_mat(ROI,:,:),3);
-%     errors = nanstd(ISC_mat(ROI,:,:),3)/sqrt(N);
+    %     y = nanmean(ISC_mat(ROI,:,:),3);
+    %     errors = nanstd(ISC_mat(ROI,:,:),3)/sqrt(N);
     
-    figsize = [100 100 400 375]; barwidth = .5; 
+    figsize = [100 100 400 375]; barwidth = .5;
     figure('Units', 'pixels', 'Position', figsize);
     bar(x,y,barwidth,'facecolor', barcolor); hold on;
     errorbar(x,y,errors,'k.', 'LineWidth', 1)
     
     xlabel('Condition'); ylabel('ISC by condition (r)'); title([ROIs{ROI}]); xlim([.3 4.7]); ylim([0 .6]); set(gca, 'XTickLabel', control_conditions, 'FontSize', 16, 'FontName', 'Helvetica');
     print(gcf, '-dtiff', ['../figures/ISC/ISC by condition (Control conds), rep' num2str(control_reps_to_include) '_' group ' group, (' ROIs{ROI} ')_' preproc_type '_' preproc_params '_nTRs_cropped=' num2str(n_cropped_TRs) '.tif']);
-
+    
     
 end
